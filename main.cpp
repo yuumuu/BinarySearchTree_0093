41b5337e0abacfd1feeb1e60ed68f75367b3747a
yuumuu <haidar.alhazen93@gmail.com>
2025-06-26 18:10:36 +0700

    Node Class Created

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class Node {
+    public:
+        int info;
+        Node *left;
+        Node *right;
+
+        // Constructor for the node class
+        Node() {
+            left = NULL; // init left to null
+            right = NULL; // init left to null
+        }
+};
+
 int main() {
     return 0;
 }
