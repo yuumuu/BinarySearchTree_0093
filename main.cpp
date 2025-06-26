6d527078c4d780b443b2f875936082404ad065f4
yuumuu <haidar.alhazen93@gmail.com>
2025-06-26 18:30:41 +0700

    Insert Node #4

--- main.cpp
+++ main.cpp
@@ -50,6 +50,15 @@ class BinaryTree {
                 // 5b: exit
                 return;
             }
+
+            // step 6: if the value in the data field of new node is less than that of parent
+            if (x < parent->info) {
+                // 6a: make the left child of parent point to the new node
+                parent->left = nb;
+
+                // 6b: exit
+                return;
+            }
         }
 };
 
